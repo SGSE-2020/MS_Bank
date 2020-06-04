674e657c0f2205605c674ffc3debd72afc8e628c
Fabian Husemann <fabian@husemann-web.de>
2020-06-04 14:51:51 +0200

    Kontoeinsicht hinzugefügt

---
+++ frontend/src/app/scripts/account.js
@@ -0,0 +1,11 @@
+
+
+
+function showAccountDetails(){
+  console.log("OnChange");
+  let panel = document.getElementById("account_panel");
+  panel.hidden = false;
+
+  let amount_lable = document.getElementById("account_amount");
+  amount_lable.hidden = false;
+}
